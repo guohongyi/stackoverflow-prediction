0783dbc5b9f2b452aa46a712112f7599fb2ccc15
Jasmeet KAUR <kaur.jasmeet1708@gmail.com>
2020-03-27 10:36:45 -0400

    working prediction

--- prediction/app.py
+++ prediction/app.py
@@ -7,6 +7,16 @@ import tensorflow as tf
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import ast
+
+from bs4 import BeautifulSoup
+import lxml
+import re
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import ToktokTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
+
 import h5py
 
 
@@ -16,7 +26,13 @@ tag_encoder = MultiLabelBinarizer()
 tag_model = tf.keras.models.load_model('model-tag-new.h5',custom_objects={'GlorotUniform': tf.keras.initializers.glorot_uniform()})
 time_model = tf.keras.models.load_model('model_Time.h5',custom_objects={'GlorotUniform': tf.keras.initializers.glorot_uniform()})
 
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 data_tag = pd.read_csv('dataTag.csv')
+token = ToktokTokenizer()
+lemma = WordNetLemmatizer()
+stop_words = set(stopwords.words("english"))
 
 
 # loading
@@ -31,6 +47,37 @@ data_tag['tags'] = data_tag['tags'].apply(ast.literal_eval)
 
 
 
+
+def strip_list_noempty(mylist):
+    newlist = (item.strip() if hasattr(item, 'strip') else item for item in mylist)
+    return [item for item in newlist if item != '']
+
+def removeStopWords(text):
+    words = token.tokenize(text)
+    filtered = [w for w in words if not w in stop_words]
+    return ' '.join(map(str, filtered))
+
+def removePunctuation(text):
+    punct = '!"$%&\'()*,./:;<=>?@[\\]^_`{|}~'
+    words = token.tokenize(text)
+    punctuation_filtered = []
+    regex = re.compile('[%s]' % re.escape(punct))
+    remove_punctuation = str.maketrans(' ', ' ', punct)
+    for w in words:
+        punctuation_filtered.append(regex.sub('', w))
+
+    filtered_list = strip_list_noempty(punctuation_filtered)
+
+    return ' '.join(map(str, filtered_list))
+
+def lemmatizeWords(text):
+    words = token.tokenize(text)
+    listLemma = []
+    for w in words:
+        x = lemma.lemmatize(w, pos="v")
+        listLemma.append(x.lower())
+    return ' '.join(map(str, listLemma))
+
 @app.route('/')
 def home():
     # loadTokens()
@@ -39,11 +86,37 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
 
-    int_features = [int(x) for x in request.form.values()]
+    features = [str(x) for x in request.form.values()]
 
+    question = features[1]
+    title = features[0]
+
+    # questions = [question].apply(lambda x: BeautifulSoup(x, "lxml").get_text())
+
+    question = BeautifulSoup(question, "lxml").get_text()
+
+
+
+    # Remove stopwords, punctuation and lemmatize for text in body
+    # question = question.apply(lambda x: removeStopWords(x))
+    # question = question.apply(lambda x: removePunctuation(x))
+    # question = question.apply(lambda x: lemmatizeWords(x))
+
+    question = removeStopWords(question)
+    question = removePunctuation(question)
+    question = lemmatizeWords(question)
+
+    # # Remove stopwords, punctuation and lemmatize for title. Also weight title 3 times
+    title = removeStopWords(title)
+    title = removePunctuation(title)
+    title = lemmatizeWords(title)
+    title =  title+' '+title+' '+title
+
+
+    feature = [title + ' ' + question]
 
     # final_features = [np.array(int_features)]
-    feature = ["r convert igraph visnetwork r convert igraph visnetwork r convert igraph visnetwork i find way convert igraph visnetwork refer interactive arules arulesviz visnetwork suppose conversion igraph visnetwork result show convert visnetwork result different i try demonstrate issue use sample data data groceries library arules #pre-defined library library arules library arulesviz library visnetwork library igraph #get sample data amp get association rule data groceries rule visedges arrows visoptions highlightnearest t plot top 10 association rule via use visnetwork note for visnetwork diagram size intercept node indicate lift higher lift larger size intercept node unlike igraph diagram size intercept node indicate support colour intercept node indicate lift let compare igraph visnetwork by refer association rule table format rule no10 rule smallest lift suppose size intercept node smallest end smallest problems i try drill igdf"]
+    # feature = ["r convert igraph visnetwork r convert igraph visnetwork r convert igraph visnetwork i find way convert igraph visnetwork refer interactive arules arulesviz visnetwork suppose conversion igraph visnetwork result show convert visnetwork result different i try demonstrate issue use sample data data groceries library arules #pre-defined library library arules library arulesviz library visnetwork library igraph #get sample data amp get association rule data groceries rule visedges arrows visoptions highlightnearest t plot top 10 association rule via use visnetwork note for visnetwork diagram size intercept node indicate lift higher lift larger size intercept node unlike igraph diagram size intercept node indicate support colour intercept node indicate lift let compare igraph visnetwork by refer association rule table format rule no10 rule smallest lift suppose size intercept node smallest end smallest problems i try drill igdf"]
     #feature = ["sdk version issue failed uploading build sdk version issue failed uploading build sdk version issue failed uploading build warning itms-90725 sdk version issue this app build ios 120 sdk starting march 2019 ios apps submit app store must build ios 121 sdk later include xcode 101 laterbut im use xcode 101 sdk version 101 show screen shoot sdk version description 1 deploymenttargey2 httpsistackimgurcomupqbcpng httpsistackimgurcombznqgpng"]
 
 
@@ -91,7 +164,7 @@ def predict():
 
     output = prediction_time
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Time should be $ {}, Tag should be $ {}'.format(source_time, source_tag))
 
 @app.route('/results',methods=['POST'])
 def results():
